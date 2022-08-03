3c2c3f492f34aca391d95b51a9b1fa0cbd049c97
Jun Wei Ng <ngjw89@gmail.com>
2022-08-04 01:00:47 +0800

    base58 encode keys

--- src/hoard/fs.py
+++ src/hoard/fs.py
@@ -5,6 +5,7 @@ import yaml
 import shutil
 import logging
 import contextlib
+import base58
 from hashlib import sha1
 from pathlib import Path
 from functools import cached_property
@@ -94,6 +95,14 @@ class BaseFSHoard(Hoard):
     def __truediv__(self, partition):
         return type(self)(path=self.root, partition=partition)
 
+    @classmethod
+    def encode_key(cls, key):
+        return base58.b58encode(key.encode()).decode()
+
+    @classmethod
+    def decode_key(cls, key):
+        return base58.b58decode(key.encode()).decode()
+
 
 class FSHoard(BaseFSHoard):
 
@@ -119,13 +128,13 @@ class FSHoard(BaseFSHoard):
         cls.atomic_write(h.config_path, 'w')(lambda fh: fh.write(yaml.dump(config)))
         return h
 
-    def get_path(self, k):
-        return Path(self.data_root / str(k))
+    def get_path(self, key):
+        fn = self.encode_key(key)
+        return Path(self.data_root / fn)
 
     def keys(self):
-        for root, dirs, files in os.walk(self.data_root):
-            for f in files:
-                yield os.path.relpath(os.path.join(root, f), self.data_root)
+        for f in Path(self.data_root).iterdir():
+            yield self.decode_key(f.name)
 
 
 class HashedFSHoard(BaseFSHoard):
@@ -161,7 +170,8 @@ class HashedFSHoard(BaseFSHoard):
         h = sha1(str(x).encode())
         return int.from_bytes(h.digest(), byteorder='big')
 
-    def get_path(self, k):
+    def get_path(self, key):
+        k = self.encode_key(key)
         q = self.hash(k)
         p = self.data_root
         for i in range(self.depth):
@@ -174,4 +184,4 @@ class HashedFSHoard(BaseFSHoard):
             for f in files:
                 rp = os.path.relpath(os.path.join(root, f), self.data_root)
                 key = rp.split(os.path.sep, self.depth)[-1]
-                yield key
+                yield self.decode_key(key)
